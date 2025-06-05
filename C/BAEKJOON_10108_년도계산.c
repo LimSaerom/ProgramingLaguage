c6f2ea70ea05b28078963fb9c7ed01a9d8f2e39b
LimSaerom <lsrom325@gmail.com>
2025-06-05 20:53:37 +0900

    -
    -

---
+++ C/BAEKJOON_10108_년도계산.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int t_Year, k_Year;
+
+  scanf("%d",&t_Year);
+
+  k_Year = t_Year - 543;
+
+  printf("%d" ,k_Year);
+  
+    return 0;
+}
---
+++ C/BAEKJOON_10926_특수기호.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char input[49];
+  scanf("%s", input);
+
+  printf("%s??!",input);
+  
+    return 0;
+}
